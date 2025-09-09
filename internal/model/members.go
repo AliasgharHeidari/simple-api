1558abae9e5cad73351ebfd2773a1d498c2db320
AliasgarHeidari <aliasgharaliali1213@gmail.com>
2025-09-09 17:36:27 +0330

    added 2 more slice for endpoins

--- internal/model/members.go
+++ internal/model/members.go
@@ -1,9 +1,12 @@
 package model
 
 type Members struct {
-	Member_Info string 	`json:"member_info,omitempty"`
-	Teacher_Info string	`json:"teacher_info,omitempty"`
+	Member_Info  string `json:"member_info,omitempty"`
+	Teacher_Info string `json:"teacher_info,omitempty"`
+	Contact_Info string `json:"contact_info,omitempty"`
+	Website_Info string `json:"website_info,omitempty"`
 }
+
 var Map_teacher = map[int]Members{
 	1: {Teacher_Info: "Name: Mahmoud, Age: 32"},
 }
@@ -11,4 +14,11 @@ var Map_members = map[int]Members{
 	1: {Member_Info: "Name: Aliasghar, Age: 18"},
 	2: {Member_Info: "Name: Shahrzad, Age: 17"},
 	3: {Member_Info: "Name: AmirHossein, Age: 21"},
+}
+var Map_contact = map[int]Members{
+	1: {Contact_Info: "Email: something@example.com, Phone: 09123456789"},
+}
+
+var Map_website = map[int]Members{
+	1: {Website_Info: "https://ArvanSchool.ir"},
 }
