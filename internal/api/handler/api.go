e6c763f6130d2d7faa262d3a2803b98bde4b108a
AliasgarHeidari <aliasgharaliali1213@gmail.com>
2025-09-09 17:34:51 +0330

    fix : added 2 more func for endpoints

--- internal/api/handler/api.go
+++ internal/api/handler/api.go
@@ -1,17 +1,22 @@
 package handler
 
 import (
-	"github.com/gofiber/fiber/v2"
 	"github.com/AliasgharHeidari/simple-api/internal/model"
+	"github.com/gofiber/fiber/v2"
 )
-
-
 
 func GopherMembers(c *fiber.Ctx) error {
 	return c.JSON(model.Map_members)
 }
 
-
 func GopherTeacher(c *fiber.Ctx) error {
 	return c.JSON(model.Map_teacher)
+}
+
+func GophersContact(c *fiber.Ctx) error {
+	return c.JSON(model.Map_contact)
+}
+
+func GophersWebsite(c *fiber.Ctx) error {
+	return c.JSON(model.Map_website)
 }
